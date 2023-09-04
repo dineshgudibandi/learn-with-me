e952c75af5bca28d576810814249363e268375c8
Dinesh Gudibandi <dineshreddyy@gmail.com>
2023-09-03 20:40:55 -0500

    added professions

--- src/utils/scripts.py
+++ src/utils/scripts.py
@@ -5,7 +5,7 @@ def read_folder_and_create_json(folder_path, folder):
     path = folder_path+folder
     try:
         filenames = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-        json_data = [{'description': filename.replace(".jpg", ""), 'original': f"images/{folder}/{filename}",'thumbnail': f"images/{folder}/{filename}"} for filename in filenames]
+        json_data = [{'description': filename.replace(".jpg", "").replace(".png",""), 'original': f"images/{folder}/{filename}",'thumbnail': f"images/{folder}/{filename}"} for filename in filenames]
 
         with open(f'../../public/jsons/{folder}.json', 'w') as json_file:
             json.dump(json_data, json_file, indent=2)
@@ -15,5 +15,5 @@ def read_folder_and_create_json(folder_path, folder):
         print('Error:', e)
 
 folder_path = '../../public/images/'  # Replace with the actual folder path
-folder = 'states'
+folder = 'professions'
 read_folder_and_create_json(folder_path, folder)
